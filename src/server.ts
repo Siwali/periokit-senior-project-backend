4d543ef9fdd00137eb155c4016d2ecfff620848f
Siwali <sambuk744@gmail.com>
2026-05-13 00:23:47 +0700

    feat(api): attach GraphQL server
    - Add the Express 5 Apollo integration dependency
    - Start Apollo Server before binding the Express listener
    - Log the backend GraphQL endpoint on startup
    - Ignore generated docs output

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import "dotenv/config";
 import { env } from "./lib/env";
 import app from "./app";
+import { attachApolloServer } from "./graphql/apollo";
 
 const PORT = env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const start = async () => {
+  await attachApolloServer(app);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+  });
+};
+
+start().catch((error) => {
+  console.error("Failed to start server", error);
+  process.exit(1);
 });
